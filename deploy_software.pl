2d9f55e4ecc71e6983b296954e222103bf251091
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-01-27 14:36:30 -0600

    debug info

--- deploy_software.pl
+++ deploy_software.pl
@@ -596,8 +596,11 @@ sub install_package {
 	my $ptarget = $package_hash->{'ptarget'} || $target;
 	
 	# package is a data package ?
-	if (defined $h->{'data_target'} && definedAndTrue($package_hash->{'data'}) ) {
+	if (defined($h->{'data_target'}) && definedAndTrue($package_hash->{'data'}) ) {
 		$ptarget = $h->{'data_target'};
+		print "is data package\n";
+	} else {
+		print "is normal software package\n";
 	}
 	
 	if (substr($ptarget, -1, 1) ne '/') {
