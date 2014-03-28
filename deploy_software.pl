1d19fa976cf5ee398a94095f0ee93607c652336b
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-03-28 14:03:32 -0500

    docker

--- deploy_software.pl
+++ deploy_software.pl
@@ -398,7 +398,7 @@ sub dockerSocket {
 	
 	if ($@) {
 		if (! ref($response_content)) {
-			print STDERR "[error] unable to connect to Shock ".$self->shock_url."\n";
+			print STDERR "[error] unable to connect to socket\n";
 			return undef;
 		} elsif (exists($response_content->{error}) && $response_content->{error}) {
 			print STDERR "[error] unable to send $method request to Shock: ".$response_content->{error}[0]."\n";
