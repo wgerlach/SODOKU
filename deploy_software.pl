d69e27b09791926fe63713d7040c2a1237929f28
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-04-08 20:06:34 -0500

    dockerfile as attribute

--- deploy_software.pl
+++ deploy_software.pl
@@ -314,7 +314,7 @@ sub upload_docker_image_to_shock {
 	
 	
 	require MIME::Base64;
-	my $dockerfile_encoded = encode_base64($dockerfile);
+	my $dockerfile_encoded = MIME::Base64::encode_base64($dockerfile);
 	
 	
 	my $json = JSON->new;
