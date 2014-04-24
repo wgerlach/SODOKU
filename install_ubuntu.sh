ea1ccbe145dbe893ab8e15f177aae6cdc6d86d85
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-04-24 08:56:11 -0500

    fix install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -8,7 +8,8 @@ cpanm install git://github.com/wgerlach/USAGEPOD.git
 
 # SHOCK::Client
 mkdir -p /usr/share/perl5/SHOCK
-wget -o /usr/share/perl5/SHOCK/Client.pm https://raw.githubusercontent.com/MG-RAST/Shock/master/libs/SHOCK/Client.pm
+wget https://raw.githubusercontent.com/MG-RAST/Shock/master/libs/SHOCK/Client.pm
+mv --force Client.pm /usr/share/perl5/SHOCK/
 
 wget https://raw.githubusercontent.com/wgerlach/SODOKU/master/deploy_software.pl
 chmod +x deploy_software.pl
