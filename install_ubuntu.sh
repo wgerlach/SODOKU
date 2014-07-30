b3e965c122b8733049db9871a333f3a437daa681
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-07-30 15:50:10 -0500

    apt-get update

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,6 +2,7 @@
 set -x
 set -e
 
+apt-get update
 apt-get --force-yes -y -q install curl build-essential cpanminus git
 cpanm install JSON LWP::UserAgent LWP::Protocol::http::SocketUnixAlt Config::IniFiles File::Slurp
 cpanm install git://github.com/wgerlach/USAGEPOD.git
